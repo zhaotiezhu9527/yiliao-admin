9d2ab3fb5735bd08674b97967abde7932c98541c
water <watershui122@gmail.com>
2023-05-21 13:15:37 +0700

    会员列表 登陆日志

---
+++ yiliao-ui/src/views/business/log/index.vue
@@ -0,0 +1,332 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="用户名" prop="userName">
+        <el-input
+          v-model="queryParams.userName"
+          placeholder="请输入用户名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="登录ip" prop="ip">
+        <el-input
+          v-model="queryParams.ip"
+          placeholder="请输入登录ip"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <!-- <el-form-item label="ip详情" prop="ipDetail">
+        <el-input
+          v-model="queryParams.ipDetail"
+          placeholder="请输入ip详情"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item> -->
+      <el-form-item label="登陆时间">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 340px"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          type="datetimerange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
+        
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <!-- <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['business:log:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['business:log:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['business:log:remove']"
+        >删除</el-button>
+      </el-col> -->
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['business:log:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="logList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="id" align="center" prop="id" />
+      <el-table-column label="用户名" align="center" prop="userName" />
+      <el-table-column label="登录ip" align="center" prop="ip" />
+      <el-table-column label="ip详情" align="center" prop="ipDetail" />
+      <el-table-column label="登录时间" align="center" prop="loginTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.loginTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <!-- <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['business:log:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['business:log:remove']"
+          >删除</el-button>
+        </template> -->
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改【请填写功能名称】对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="form.userName" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="登录ip" prop="ip">
+          <el-input v-model="form.ip" placeholder="请输入登录ip" />
+        </el-form-item>
+        <el-form-item label="ip详情" prop="ipDetail">
+          <el-input v-model="form.ipDetail" placeholder="请输入ip详情" />
+        </el-form-item>
+        <el-form-item label="登录时间" prop="loginTime">
+          <el-date-picker clearable
+            v-model="form.loginTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择登录时间">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listLog, getLog, delLog, addLog, updateLog } from "@/api/business/log";
+
+export default {
+  name: "Log",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 【请填写功能名称】表格数据
+      logList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      dateRange: '',
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        userName: null,
+        ip: null,
+        ipDetail: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        userName: [
+          { required: true, message: "用户名不能为空", trigger: "blur" }
+        ],
+      },
+      pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+    };
+  },
+  created() {
+    this.getList();
+    console.log(this.queryParams)
+  },
+  methods: {
+    /** 查询【请填写功能名称】列表 */
+    getList() {
+      this.loading = true;
+      listLog(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+        this.logList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        userName: null,
+        ip: null,
+        ipDetail: null,
+        loginTime: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加【请填写功能名称】";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getLog(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改【请填写功能名称】";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateLog(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addLog(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除【请填写功能名称】编号为"' + ids + '"的数据项？').then(function() {
+        return delLog(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('business/log/export', {
+        ...this.queryParams
+      }, `log_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
---
+++ yiliao-ui/src/views/business/user/index.vue
@@ -0,0 +1,442 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="用户名" prop="userName">
+        <el-input
+          v-model="queryParams.userName"
+          placeholder="请输入用户名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="推荐人" prop="userAgent">
+        <el-input
+          v-model="queryParams.userAgent"
+          placeholder="请输入推荐人用户名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="状态" prop="userStatus">
+        <el-select v-model="queryParams.userStatus" placeholder="请选择">
+          <el-option
+            v-for="item in statusArr"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="注册时间" prop="registerTime">
+        <el-date-picker
+          v-model="dateRange"
+          style="width: 340px"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          type="datetimerange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['business:user:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['business:user:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['business:user:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['business:user:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="userList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="用户ID" align="center" prop="id" />
+      <el-table-column label="邀请码" align="center" prop="inviteCode" />
+      <el-table-column label="姓名" align="center" prop="realName" />
+      <el-table-column label="用户名" align="center" prop="userName" />
+      <el-table-column label="用户详情" align="center" prop="userName" width="150">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['business:user:query']"
+          >用户详情</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column label="USDT钱包地址" align="center" prop="walletAddr" width="200"/>
+      <el-table-column label="银行信息" align="center" prop="bankName" width="300">
+        <template slot-scope="scope">
+          <div>{{scope.row.bankName}}</div>
+          <div>{{scope.row.bankAddr}}</div>
+          <div>{{scope.row.bankCardNum}}</div>
+        </template>
+      </el-table-column>>
+      <!-- <el-table-column label="支行信息" align="center" prop="bankAddr" />
+      <el-table-column label="银行卡号" align="center" prop="bankCardNum" /> -->
+      <el-table-column label="冻结" align="center" prop="userStatus">
+        <template slot-scope="scope">
+          <span>{{scope.row.userStatus === 0 ? '正常' : '冻结' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="会员等级" align="center" prop="userLevelId" >
+        <template slot-scope="scope">
+          <span>{{scope.row.userLevelId === 0 ? '普通会员' : '未知等级' }}</span>
+        </template>
+        </el-table-column>
+      <el-table-column label="推荐人用户名" align="center" prop="userAgent" />
+      <el-table-column label="账户余额" align="center" prop="balance" />
+      <el-table-column label="注册时间" align="center" prop="registerTime" width="180"/>
+      <!-- <el-table-column label="最后修改时间" align="center" prop="modifyTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.modifyTime, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column> -->
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['business:user:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['business:user:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改【请填写功能名称】对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="form.userName" placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item label="用户余额" prop="balance">
+          <el-input v-model="form.balance" placeholder="请输入用户余额" />
+        </el-form-item>
+        <el-form-item label="真实姓名" prop="realName">
+          <el-input v-model="form.realName" placeholder="请输入真实姓名" />
+        </el-form-item>
+        <el-form-item label="身份证号码" prop="idCard">
+          <el-input v-model="form.idCard" placeholder="请输入身份证号码" />
+        </el-form-item>
+        <el-form-item label="登录密码" prop="loginPwd">
+          <el-input v-model="form.loginPwd" placeholder="请输入登录密码" />
+        </el-form-item>
+        <el-form-item label="支付密码" prop="payPwd">
+          <el-input v-model="form.payPwd" placeholder="请输入支付密码" />
+        </el-form-item>
+        <el-form-item label="邀请码" prop="inviteCode">
+          <el-input v-model="form.inviteCode" placeholder="请输入邀请码" />
+        </el-form-item>
+        <el-form-item label="钱包地址" prop="walletAddr">
+          <el-input v-model="form.walletAddr" placeholder="请输入钱包地址" />
+        </el-form-item>
+        <el-form-item label="银行卡号" prop="bankCardNum">
+          <el-input v-model="form.bankCardNum" placeholder="请输入银行卡号" />
+        </el-form-item>
+        <el-form-item label="银行名称" prop="bankName">
+          <el-input v-model="form.bankName" placeholder="请输入银行名称" />
+        </el-form-item>
+        <el-form-item label="开户行地址" prop="bankAddr">
+          <el-input v-model="form.bankAddr" placeholder="请输入开户行地址" />
+        </el-form-item>
+        <el-form-item label="用户等级id" prop="userLevelId">
+          <el-input v-model="form.userLevelId" placeholder="请输入用户等级id" />
+        </el-form-item>
+        <el-form-item label="上级代理用户名" prop="userAgent">
+          <el-input v-model="form.userAgent" placeholder="请输入上级代理用户名" />
+        </el-form-item>
+        <el-form-item label="是否实名(0:已实名 1:未实名)" prop="isRealName">
+          <el-input v-model="form.isRealName" placeholder="请输入是否实名(0:已实名 1:未实名)" />
+        </el-form-item>
+        <el-form-item label="注册时间" prop="registerTime">
+          <el-date-picker clearable
+            v-model="form.registerTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择注册时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="注册Ip" prop="registerIp">
+          <el-input v-model="form.registerIp" placeholder="请输入注册Ip" />
+        </el-form-item>
+        <el-form-item label="上次登录时间" prop="lastTime">
+          <el-date-picker clearable
+            v-model="form.lastTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择上次登录时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="上次登录IP" prop="lastIp">
+          <el-input v-model="form.lastIp" placeholder="请输入上次登录IP" />
+        </el-form-item>
+        <el-form-item label="最后修改时间" prop="modifyTime">
+          <el-date-picker clearable
+            v-model="form.modifyTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择最后修改时间">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listUser, getUser, delUser, addUser, updateUser } from "@/api/business/user";
+
+export default {
+  name: "User",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 【请填写功能名称】表格数据
+      userList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 时间
+      dateRange:[],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        userName: null,
+        userStatus: null,
+        userAgent: null,
+        registerTime: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        userName: [
+          { required: true, message: "用户名不能为空", trigger: "blur" }
+        ],
+      },
+      pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+        statusArr: [
+          { label: '全部', value: ''},
+          { label: '正常', value: 0},
+          { label: '已冻结', value: 1},
+        ],//银行卡状态
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询【请填写功能名称】列表 */
+    getList() {
+      this.loading = true;
+      listUser(this.queryParams).then(response => {
+        this.userList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        userName: null,
+        balance: null,
+        realName: null,
+        idCard: null,
+        loginPwd: null,
+        payPwd: null,
+        inviteCode: null,
+        walletAddr: null,
+        bankCardNum: null,
+        bankName: null,
+        bankAddr: null,
+        userStatus: null,
+        userLevelId: null,
+        userAgent: null,
+        isRealName: null,
+        registerTime: null,
+        registerIp: null,
+        lastTime: null,
+        lastIp: null,
+        modifyTime: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加【请填写功能名称】";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getUser(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改【请填写功能名称】";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateUser(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addUser(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除【请填写功能名称】编号为"' + ids + '"的数据项？').then(function() {
+        return delUser(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('business/user/export', {
+        ...this.queryParams
+      }, `user_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
